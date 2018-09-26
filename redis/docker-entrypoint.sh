b73d3a88129e5e49b12ba3673bab4ac554dd2b1a
prannoy <prannoy.mulmi@gmail.com>
2018-09-26 16:16:46 +0200

    Fixed redis issue

--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-mkdir /opt/bitnami/redis/etc/redis.conf
+
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
